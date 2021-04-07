debe19f2398ea5e1a45ec24fbae98f0ac7b38930
Jolver Andre Gil Quevedo <devneonix@gmail.com>
2021-04-06 19:43:39 -0500

    Creando funcion login del servicio Auth

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {User} from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor() {
+  }
 
-  
+  login(email: string, password: string): User {
+    const users = JSON.parse(localStorage.getItem('users'));
+    let auth: User = null;
+    // @ts-ignore
+    for (const user: User of users) {
+      if (user.email === email && user.password === password) {
+        auth = user;
+      }
+    }
+    return auth;
 
+  }
 }
