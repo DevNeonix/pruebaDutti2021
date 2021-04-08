733fe143fa3d3e47413fd73ba3dffcc2619ce80a
Jolver André Gil Quevedo <devneonix@gmail.com>
2021-04-08 12:12:01 -0500

    Titulos y nombre de usuario
    se agregó los titulos por cada página, y se agrego al navbar el nombre del usuario autenticado

--- src/app/pages/principal/principal.component.ts
+++ src/app/pages/principal/principal.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {User} from "../../core/interfaces/user";
 
 @Component({
   selector: 'app-principal',
@@ -7,8 +8,11 @@ import {Router} from "@angular/router";
   styleUrls: ['./principal.component.scss']
 })
 export class PrincipalComponent implements OnInit {
+  user: User;
 
   constructor(private router: Router) {
+    const luser = localStorage.getItem('user');
+    this.user = JSON.parse(luser);
   }
 
   ngOnInit(): void {
